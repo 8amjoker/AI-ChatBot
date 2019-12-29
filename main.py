f0b255c050126d461c7537cff9245e8f6da33f6e
KingCobra2018 <kcobra557@gmail.com>
2019-12-29 20:01:26 +0000

    Minor fixes to typo

--- main.py
+++ main.py
@@ -14,19 +14,44 @@ with open('data.json') as file:
 words = []
 labels = []
 docs_x = []
-docs_y =[]
+docs_y = []
 
 for item in data['data']:
     for pattern in item['patterns']:
         splitword = nltk.word_tokenize(pattern)
         words.extends(splitword)
-        docs_x.append(pattern)
+        docs_x.append(splitword)
         docs_y.append(item['tag'])
 
     if item['tag'] not in labels:
         labels.append(item['tag'])
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != '?']
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    splitword = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in splitword:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
